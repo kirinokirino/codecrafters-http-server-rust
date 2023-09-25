01cbc58edc9ea6047f69534ea31f01ebefa2acf2
kirinokirino <kirinokirino2501@gmail.com>
2023-09-25 17:08:59 +0300

    Response 200.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
 // Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use std::net::TcpListener;
+
+use std::io::{Write, Read};
+use std::fmt::{self, Display};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                stream.read_to_end(&mut Vec::new()).unwrap();
+                let _ = stream.write_all(response(HttpCode::Ok).as_bytes());
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
+
+enum HttpCode {
+  Ok
+}
+
+impl Display for HttpCode {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      HttpCode::Ok => write!(f, "200 OK"),
+    }
+  }
+}
+
+fn response(code: HttpCode) -> String {
+  format!("HTTP/1.1 {}\r\n\r\n", code)
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::*;
+
+  #[test]
+  fn test_ok_response() {
+    assert_eq!("HTTP/1.1 200 OK\r\n\r\n", response(HttpCode::Ok));
+  }
 }
